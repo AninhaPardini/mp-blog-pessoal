4eb8c2bf12798bafa261d5f77b12a50033b59044
AnaPardini <ana@niutech.app.br>
2024-07-27 02:09:47 -0300

    First Configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,48 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      'mandy': {
+        '50': '#fef2f3',
+        '100': '#fde6e7',
+        '200': '#fbd0d5',
+        '300': '#f7aab2',
+        '400': '#f27a8a',
+        '500': '#ea546c',
+        '600': '#d5294d',
+        '700': '#b31d3f',
+        '800': '#961b3c',
+        '900': '#811a39',
+        '950': '#48091a',
       },
+
+      'saffron': {
+        '50': '#fdfae9',
+        '100': '#fcf5c5',
+        '200': '#faea8e',
+        '300': '#f7d74d',
+        '400': '#f3c52c',
+        '500': '#e2a910',
+        '600': '#c3820b',
+        '700': '#9c5d0c',
+        '800': '#814a12',
+        '900': '#6e3c15',
+        '950': '#401e08',
+      },
+
+      'white': {
+        '50': '#ffffff',
+        '100': '#efefef',
+        '200': '#dcdcdc',
+        '300': '#bdbdbd',
+        '400': '#989898',
+        '500': '#7c7c7c',
+        '600': '#656565',
+        '700': '#525252',
+        '800': '#464646',
+        '900': '#3d3d3d',
+        '950': '#292929',
+      },
+    
     },
   },
   plugins: [],
