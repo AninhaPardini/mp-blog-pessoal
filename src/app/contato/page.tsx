8d7e1b5412e918b095b1408f103793b21487e5ac
AninhaPardini <aninhapardini.work@gmail.com>
2024-07-27 04:42:02 -0300

    feat: Adicionando formatação da pag de contato

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,9 @@
+export default function Contato() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="container">
+        Here is Contact page.
+      </div>
+    </div>
+  );
+}
