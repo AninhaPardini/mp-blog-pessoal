6a4eaf72eebf0788a117b7cf734ae456b4b27ca0
AninhaPardini <aninhapardini.work@gmail.com>
2024-07-27 04:44:06 -0300

    feat: Adicionando formatação da pag de contato

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,9 @@
+export default function Sobre() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="container">
+        Here is About page.
+      </div>
+    </div>
+  );
+}
